1ec77dd474f81ac78c78e70044047ed8df30e1bb
Kartik Pawar <nikitashimpi5302@gmail.com>
2022-08-26 16:14:28 +0530

    Bug fic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
-import { Audio } from "react-loader-spinner";
+
 import Data from "./components/Data";
 
 const App = () => {
-  const [loading, setLoading] = useState(!true);
   return (
     <div className="text-3xl tere h-full">
       <Navbar />
       <div className="flex items-center justify-center  h-full">
-        {loading ? (
-          <Audio
-            height="80"
-            width="80"
-            radius="9"
-            color="green"
-            ariaLabel="three-dots-loading"
-            wrapperStyle
-            wrapperClass
-          />
-        ) : (
-          <Data />
-        )}
+        <Data />
       </div>
     </div>
   );
